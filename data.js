4d5bb259591d1a51b42ea40ee15be931df11d05c
Olena-Smilenko <127014096+Olena-Smilenko@users.noreply.github.com>
2023-09-20 19:17:40 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,281 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-drauen",
+      "name": "Draußen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 2.7555876328462947,
+        "pitch": -0.08485481356728641,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.703989892168062,
+          "pitch": 0.024895809919811995,
+          "rotation": 0,
+          "target": "1-zimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-zimmer",
+      "name": "Zimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -1.6532015314868307,
+        "pitch": 0.13074126206318937,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.114451581085998,
+          "pitch": 0.0855707639205896,
+          "rotation": 0,
+          "target": "3-kche"
+        },
+        {
+          "yaw": 1.423860407177914,
+          "pitch": 0.1788813112355392,
+          "rotation": 0,
+          "target": "2-zweites-bett"
+        },
+        {
+          "yaw": 0.2655412373466035,
+          "pitch": 0.011703781653062961,
+          "rotation": 0,
+          "target": "5-terrasse"
+        },
+        {
+          "yaw": 2.7727255630459933,
+          "pitch": 0.09453040849838246,
+          "rotation": 0,
+          "target": "4-badezimmer"
+        },
+        {
+          "yaw": -2.9507079731565646,
+          "pitch": 0.08565910399858723,
+          "rotation": 0,
+          "target": "0-drauen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-zweites-bett",
+      "name": "Zweites Bett",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -2.240267590200663,
+        "pitch": 0.23730641100895333,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4075470895524678,
+          "pitch": 0.26628195354432194,
+          "rotation": 0,
+          "target": "1-zimmer"
+        },
+        {
+          "yaw": -0.20834621628265637,
+          "pitch": 0.16752396609418874,
+          "rotation": 0,
+          "target": "5-terrasse"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kche",
+      "name": "Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.009774216862712848,
+        "pitch": 0.29732798075246336,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3488312617374767,
+          "pitch": 0.36571006848084053,
+          "rotation": 0,
+          "target": "1-zimmer"
+        },
+        {
+          "yaw": -1.4232997500266684,
+          "pitch": 0.0193661900375659,
+          "rotation": 0,
+          "target": "5-terrasse"
+        },
+        {
+          "yaw": -0.6384305641669847,
+          "pitch": 0.033616661404620274,
+          "rotation": 0,
+          "target": "2-zweites-bett"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-badezimmer",
+      "name": "Badezimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 2.0093183155633376,
+        "pitch": 0.15255871690979994,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7197369590488893,
+          "pitch": 0.07810220049105432,
+          "rotation": 0,
+          "target": "1-zimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-terrasse",
+      "name": "Terrasse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -0.8592557308860638,
+        "pitch": 0.09149653291112969,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.559211978898581,
+          "pitch": 0.060434807944002245,
+          "rotation": 0,
+          "target": "1-zimmer"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
